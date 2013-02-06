49604bdf1052eeac5d4daeafc67ea4052b3f4787
Arturo Fernandez <arturo@bsnux.com>
2013-02-06 13:32:38 +0100

    Adding new properties and unicode for models

--- huey/djhuey/models.py
+++ huey/djhuey/models.py
@@ -1,3 +1,5 @@
+import pickle
+
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
@@ -9,9 +11,29 @@ class BackgroundTask(models.Model):
     key = models.CharField(max_length=255, verbose_name=_(u'Command'), unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    @property
+    def task_name(self):
+        try:
+            return pickle.loads(self.data)[1]
+        except:
+            return None
+
+    def __unicode__(self):
+        return unicode('{0} - {1}'.format(self.name, self.key))
+
 
 class BackgroundResultTask(models.Model):
     name = models.CharField(max_length=255, verbose_name=_(u'Queue name'))
     result = models.TextField()
     key = models.ForeignKey(BackgroundTask, to_field='key')
     created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def task_result(self):
+        try:
+            return pickle.loads(self.result)
+        except:
+            return None
+
+    def __unicode__(self):
+        return unicode('{0} - {1}'.format(self.name, self.key))
